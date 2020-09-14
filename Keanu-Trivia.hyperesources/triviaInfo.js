487939ce61716509d1f555d231816544237b8653
yfaysal123 <46056309+yfaysal123@users.noreply.github.com>
2020-09-13 22:29:11 -0500

    Add files via upload

---
+++ Keanu-Trivia.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"KEANU REEVES TRIVIA";
+	window.questions = [		"WHAT YEAR WAS KEANU REEVES BORN", 
+								"WHAT WAS HIS MOST RECENT MOVIE",
+								"WHAT CHARACTER DID KEANU REEVES PLAY IN THE MATRIX", 
+								"WHAT MOVIE OF KEANU INVOLVED HIS DOG DYING",
+								"WHAT IS THE NAME OF THE MOVIE THAT STARRED KEANU AND SANDRA BULLOCK", 
+								"WHAT OTHER PROFESSION BESIDES ACTING DOES KEANU REEVES DO",
+								"WHAT IS KEANU REEVES FAVORITE COLOR",
+								"WHAT IS THE HEIGHT OF KEANU REEVES",
+								"WHAT IS KEANU REEVES NATIONALITY",
+								"WHAT IS KEANU REEVES FAVORITE VEHICLE" ];
+	window.correctAnswer = [ 	"A", "C", "A", "B", "E", "D", "C", "B", "D", "E" ]; 
+	window.answerA = [ 			"1964", 
+								"John Wick 3", 
+								"Neo", 
+								"The Bad Batch", 
+								"Die Hard",
+								"Painting",
+								"Red",
+								"6'3",
+								"American",
+								"Helicopter" ];
+	window.answerB = [ 			"1978", 
+								"Speed", 
+								"Morpheus", 
+								"John Wick",
+								"Green Mile",
+								"Poetry",
+								"Green",
+								"6'1",
+								"Argentines",
+								"Planes" ];
+	window.answerC = [ 			"1965",
+								"Bill and Ted",
+								"Cypher", 
+								"Siberia", 
+								"Braveheart",
+								"Bird Watching",
+								"Blue",
+								"5'9",
+								"Italian",
+								"Convertible" ];
+	window.answerD = [ 			"1980",  
+								"Toy Story",
+								"Tank", 
+								"A Happening",
+								"Mission Impossible",
+								"Musician",
+								"Purple",
+								"5'8",
+								"Canadian",
+								"Truck" ];
+	window.answerE = [ 			"1977", 
+								"Spongebob", 
+								"Trinity", 
+								"The Whole Truth", 
+								"Speed",
+								"Sculpter",
+								"Yellow",
+								"6'0",
+								"French",
+								"Motorcycle" ];
+	window.response = [ 		"KEANU REEVES WAS BORN SEPTEMBER 2, 1964", 
+								"KEANU REEVES MOST RECENT MOVIE IS BILL AND TED FACE THE MUSIC", 
+								"THE CHARACTER KEANU REEVES PLAYED WAS NEO", 
+								"THE MOVIE WHICH INVOLVED A DOG DYING IS JOHN WICK CHAPTER 1", 
+								"THE MOVIE THAT STARRED BOTH KEANU AND SANDRA IS SPEED",
+								"KEANU REEVES OTHER PROFESSION IS BEING A MUSICIAN",
+								"KEANU REEVES FAVORITE COLOR IS BLUE",
+								"KEANU REEVES IS 6'1",
+								"KEANU REEVES IS CANADIAN",
+								"KEANU REEVES FAVORITE VEHILE IS THE MOTORCYCLE" ];
+							
+}
+						
